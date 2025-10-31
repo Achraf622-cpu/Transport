417e09f9398c12bd3767f516f9379ecc75d20164
Achraf Hanzaz <hanzazachraf581@gmail.com>
2025-10-31 14:49:44 +0100

    NM-21: Ajout de entity warehouse

---
+++ src/main/java/org/example/transport/entity/Warehouse.java
@@ -0,0 +1,49 @@
+package org.example.transport.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+/**
+ * Entity representing a warehouse (depot)
+ */
+@Entity
+@Table(name = "warehouses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Warehouse name is required")
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank(message = "Address is required")
+    @Column(nullable = false)
+    private String address;
+
+    @NotNull(message = "Latitude is required")
+    @Column(nullable = false)
+    private Double latitude;
+
+    @NotNull(message = "Longitude is required")
+    @Column(nullable = false)
+    private Double longitude;
+
+    @NotNull(message = "Opening time is required")
+    @Column(nullable = false)
+    private LocalTime openingTime;
+
+    @NotNull(message = "Closing time is required")
+    @Column(nullable = false)
+    private LocalTime closingTime;
+}
